89c9ce74fc7cd3cb42206b438abbc0ce9b5de171
Sankeerth <133137769+sankeerth3399@users.noreply.github.com>
2026-09-09 17:48:49 +0530

    Update Router.jsx 12

--- src/app/Router.jsx
+++ src/app/Router.jsx
@@ -6,7 +6,7 @@ import Fly from "@/features/fly/pages/fly.jsx"
 import Mession from "@/features/mession/pages/MissionPage.jsx"
 import Plan from "@/features/plan/pages/Plan.jsx"
 import Waypts from "@/features/waypts/pages/Waypts.jsx"
-import Logs from "@/features/logs/pages/logs.jsx"
+import Logs from "@/features/logs/pages/Logs.jsx"
 import Landing from "@/features/landing/pages/landingPage.jsx"
 import Login from "@/features/auth/pages/Login.jsx"
 
